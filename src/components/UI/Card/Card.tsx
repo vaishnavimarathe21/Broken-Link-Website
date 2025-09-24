377f090d7d163e6f72a22c7e334ed39bc76dadb0
Irena Varga <irenva101@gmail.com>
2025-09-24 20:38:33 +0200

    Resolved PR comments

--- src/components/UI/Card/Card.tsx
+++ src/components/UI/Card/Card.tsx
@@ -1,17 +1,17 @@
-import React, { ReactNode } from 'react';
-import { Card as MantineCard } from '@mantine/core';
+import React, { CSSProperties, ReactNode } from 'react';
+import { CardProps, Card as MantineCard } from '@mantine/core';
 import { cardStyles } from './styles';
 
-interface SharedCardProps {
+interface SharedCardProps extends Omit<CardProps, "style"> {
+  style?: CSSProperties;
   children: ReactNode;
-  className?: string;
 }
 
-export const Card = ({ children, className }: SharedCardProps) => {
-  const style = cardStyles.default; 
+export const Card = ({ style = {}, children, ...props }: SharedCardProps) => {
+  const defaultStyle = cardStyles.default;
 
-  return (
-    <MantineCard style={style} className={className}>
+    return (
+    <MantineCard style={{ ...defaultStyle, ...style }} {...props}>
       {children}
     </MantineCard>
   );
--- src/components/UI/Card/styles.ts
+++ src/components/UI/Card/styles.ts
@@ -6,7 +6,7 @@ export const cardStyles = {
     padding: '1rem',
     borderRadius: '0.5rem',
     backgroundColor: colors.primary[5],
-    boxShadow: '0 1px 3px rgba(0,0,0,0.1)',
+    boxShadow: `0 1px 3px ${colors.primary[1]}`,
     color: colors.cyan[0],
   },
 };
