76db2f40f8641a23a7ce6cd1239471867eb4db57
R0EYK <roeyk70@gmail.com>
2025-09-26 13:02:17 +0300

    Added custom size prop

--- src/components/UI/Typography/Typography.tsx
+++ src/components/UI/Typography/Typography.tsx
@@ -2,21 +2,34 @@ import { CSSProperties } from 'react';
 import { Text as MantineText, TextProps } from '@mantine/core';
 import { typographyVariants } from './styles';
 
-interface SharedTypographyProps extends Omit<TextProps, 'variant' | 'style'> {
+type CustomSize = 'small' | 'medium' | 'large' | 'extralarge';
+
+const sizeMapper: Record<CustomSize, string> = {
+  small: 'sm',
+  medium: 'md',
+  large: 'lg',
+  extralarge: 'xl',
+};
+
+interface SharedTypographyProps extends Omit<TextProps, 'variant' | 'style' | 'size'> {
   variant?: keyof typeof typographyVariants;
   children?: React.ReactNode;
   style?: CSSProperties;
+  size?: CustomSize;
 }
 
 export const Typography = ({
   style,
   variant = 'primary',
+  size = 'medium',
   children,
   ...props
 }: SharedTypographyProps) => {
   const variantStyle = typographyVariants[variant];
+  const mappedSize = sizeMapper[size];
+
   return (
-    <MantineText style={{ ...variantStyle, ...style }} {...props}>
+    <MantineText size={mappedSize} style={{ ...variantStyle, ...style }} {...props}>
       {children}
     </MantineText>
   );
