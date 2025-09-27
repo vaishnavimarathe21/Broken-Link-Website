ccd9017f57d3f195da3e146e8ffffd2a806b842e
Tamir Abutbul <1tamir198@gmail.com>
2025-09-27 23:25:21 +0300

    Removed comment

--- src/i18.tsx
+++ src/i18.tsx
@@ -1,8 +1,6 @@
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
 
-// import en from '../public/locals/en.json';
-
 const res = await fetch("/locals/en.json");
 const en = await res.json();
 
