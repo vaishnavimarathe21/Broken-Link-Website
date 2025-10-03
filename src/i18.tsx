edf71d36b705b38d35a1dd4edaa2e7dd6d3c0d49
Tamir Abutbul <1tamir198@gmail.com>
2025-10-03 19:46:22 +0300

    Add Import to the file to fix build

--- src/i18.tsx
+++ src/i18.tsx
@@ -1,31 +1,30 @@
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
+import deTranslations from '../public/locals/de.json';
 import enTranslations from '../public/locals/en.json';
 import esTranslations from '../public/locals/es.json';
 import ptBrTranslations from '../public/locals/ptbr.json';
 
-i18n
-    .use(initReactI18next)
-    .init({
-        resources: {
-            en: {
-            translation: enTranslations,
-        },
-        es: {
-            translation: esTranslations,
-        },
-        'pt-BR': {
-            translation: ptBrTranslations,
-        },
-        de: {
-            translation: deTranslations,
-        },
+i18n.use(initReactI18next).init({
+  resources: {
+    en: {
+      translation: enTranslations,
     },
-        lng: 'en',
-        fallbackLng: 'en',
-        interpolation: {
-            escapeValue: false,
-        },
-    });
+    es: {
+      translation: esTranslations,
+    },
+    'pt-BR': {
+      translation: ptBrTranslations,
+    },
+    de: {
+      translation: deTranslations,
+    },
+  },
+  lng: 'en',
+  fallbackLng: 'en',
+  interpolation: {
+    escapeValue: false,
+  },
+});
 
 export default i18n;
